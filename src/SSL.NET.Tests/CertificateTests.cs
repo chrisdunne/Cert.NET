ba42e88baf997147f8feb61cac9d4086f5d4c48f
chris <chrisdunne88@outlook.com>
2019-01-14 22:14:07 +0000

    setup of Certificate class

---
+++ src/SSL.NET.Tests/CertificateTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Cert.NET.Tests
+{
+    [TestFixture]
+    public class CertificateTests
+    {
+        [Test]
+        public void Certificate_WhenInstantiated_CreateCert()
+        {
+            string name = "www.chrisdunne.net";
+            Certificate cert = new Certificate(name);
+
+            Assert.IsInstanceOf(typeof(Certificate), cert);
+        }
+
+        [Test]
+        public void Certificate_WhenGetPublic_ReturnKey()
+        {
+            Certificate cert = new Certificate("www.chrisdunne.net");
+            string output = cert.GetPublicKey();
+
+            Assert.IsNotEmpty(output, "Public Key is empty");
+        }
+
+        [Test]
+        public void Certificate_WhenGetPrivate_ReturnKey()
+        {
+            Certificate cert = new Certificate("www.chrisdunne.net");
+            string output = cert.GetPrivateKey();
+
+            Assert.IsNotEmpty(output, "Private Key is empty");
+        }
+
+        [Test]
+        public void Certificate_WhenGetCAB_ReturnKey()
+        {
+            Certificate cert = new Certificate("www.chrisdunne.net");
+            string output = cert.GetCABundle();
+
+            Assert.IsNotEmpty(output, "CA Bundle is empty");
+        }
+
+        [Test]
+        public void Certificate_WhenDeleted_CheckExists()
+        {
+            Assert.Fail("Not Implemented");
+        }
+
+        [Test]
+        public void Certificate_WhenCreate_CheckExists()
+        {
+            Assert.Fail("Not Implemented");
+        }
+    }
+}
---
+++ src/SSL.NET/Certificate.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Cert.NET
+{
+    public class Certificate
+    {
+        private X509Certificate2 _cert;
+
+        public Certificate(string name)
+        {
+            _cert = new X509Certificate2();
+        }
+
+        public string GetPublicKey()
+        {
+            return "Public Key";
+        }
+
+        public string GetPrivateKey()
+        {
+            return "Private Key";
+        }
+
+        public string GetCABundle()
+        {
+            return "CA Bundle";
+        }
+    }
+}
